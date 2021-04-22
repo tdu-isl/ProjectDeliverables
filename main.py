88a015cf786b644efe013c61adcf2eecab9778e4
18fi552-takumi-matsumoto <takumi-matsumoto@isl.im.dendai.ac.jp>
2021-04-22 12:16:46 +0900

    今日の成果

---
+++ main.py
@@ -0,0 +1,42 @@
+
+from flask import Flask, request, render_template
+from UserModel import User
+from setting import session
+from sqlalchemy import *
+from sqlalchemy.orm import *
+
+# appという名前でFlaskのインスタンスを作成
+app = Flask(__name__)
+
+
+# 登録処理
+@app.route('/', methods=["POST"])
+def register_record():
+
+    name = request.form['name']
+
+    session.add(User(name))
+
+    session.commit()
+
+    return render_template("hello.html", name=name, message="登録完了しました！")
+
+# 取得処理
+@app.route('/', methods=["GET"])
+def fetch_record():
+
+    name = request.args.get('name')
+
+    db_user = session.query(User.name).\
+        filter(User.name == name).\
+        all()
+
+    if len(db_user) == 0:
+        message = "登録されていません。"
+    else:
+        message = "登録されています。"
+
+    return render_template("index.html", name=name, message=message)
+
+if __name__ == '__main__':
+    app.run()
---
+++ setting.py
@@ -0,0 +1,29 @@
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+
+# postgresqlのDBの設定
+DATABASE = "postgresql://postgres:chancy_anecdote_smear@wsp2020.yoitsu.dev:5432/videoInfo"
+
+# Engineの作成
+ENGINE = create_engine(
+    DATABASE,
+    encoding="utf-8",
+    # TrueにするとSQLが実行される度に出力される
+    echo=True
+)
+
+# Sessionの作成
+session = scoped_session(
+    # ORM実行時の設定。自動コミットするか、自動反映するなど。
+    sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=ENGINE
+    )
+)
+
+# modelで使用する
+Base = declarative_base()
+Base.query = session.query_property()
