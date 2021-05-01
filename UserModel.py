ab51d2f4d14ef8c799166caff3cef971549c878f
18fi552-takumi-matsumoto <takumi-matsumoto@isl.im.dendai.ac.jp>
2021-05-01 14:41:50 +0900

    進捗

--- UserModel.py
+++ UserModel.py
@@ -6,22 +6,31 @@ from setting import ENGINE
 
 
 #動画のインスタンス
-class videoInfo(BASE):
+class videoInfo(Base):
     __tablename__ = 'videoInfo'
-    id = Column(String, primary_key=True, autoincrement=True)
-    title = Column(String)
-    channel = Column(String)
+    id = Column(String(20), primary_key=True)
+    title = Column(String(50))
+    channel = Column(String(30))
+    description=Column(String(500))
     viewCount = Column(Integer)
-    videoUrl = Column(String)
-    imageUrl = Column(String)
+    videoURL = Column(String(200))
+    imageURL = Column(String(200))
+    kind = Column(String(20))
 
-    def __init__(self,title,channel,viewCount,videoUrl,imageUrl):
+    def __init__(self,id,title,channel,description,viewCount,videoURL,imageURL,kind):
+        self.id=id
         self.title=title
         self.channel=channel
+        self.description=description
         self.viewCount=viewCount
-        self.videowUrl=videoUrl
-        self.imageUrl=imageUrlS
+        self.videoURL=videoURL
+        self.imageURL=imageURL
+        self.kind=kind
     
+    def __repr__(self):
+        return "id:"+self.id+"タイトル:"+self.title+",チャンネル名:"+self.channel+",概要欄:"+self.description+",再生数:"+str(self.viewCount)+",URL:"+self.videoURL+",サムネ:"+self.imageURL+",動画サイト名:"+self.kind
+    
+
   
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,42 +1,92 @@
-
-from flask import Flask, request, render_template
-from UserModel import User
+from flask import Flask, request, render_template , redirect
+from UserModel import videoInfo
 from setting import session
 from sqlalchemy import *
 from sqlalchemy.orm import *
+import json
+from bs4 import BeautifulSoup
+import requests
+
+video = []
+#インスタンス化し、videoリストに入れる(nico)
+def nico_res(url):
+    req = requests.get(url)
+    res = json.loads(req.text)
+    
+    for i in range(len(res["data"])):
+            chaUrl = "https://ext.nicovideo.jp/api/getthumbinfo/"+res["data"][i]["contentId"]
+            chareq = requests.get(chaUrl)
+            res["data"][i]["description"] = res["data"][i]["description"].replace("<br />","")
+            resvideo = videoInfo(res["data"][i]["contentId"],res["data"][i]["title"],chareq.text[chareq.text.find('<user_nickname>')+15:chareq.text.find('</user_nickname>')],res["data"][i]["description"],res["data"][i]["viewCounter"],"https://nico.ms/"+res["data"][i]["contentId"],res["data"][i]["thumbnailUrl"],"ニコニコ動画")
+            video.append(resvideo)
+
+#インスタンス化し、videoリストに入れる(Youtube)
+def you_res(word):
+    # 動画情報取得
+    url = "https://www.googleapis.com/youtube/v3/search"
+    # パラメータの設定
+    params = {'type': 'video',
+            'part': 'snippet',
+            'q': word,
+             'key': 'AIzaSyDP1Ya1uZetxtl4D4lkbHR5MY-xPpGvVE8',
+            'maxResults': '1',
+            'regionCode': 'jp',
+             'fields': 'items(id(videoId),snippet(title,description,channelTitle,thumbnails(high(url))))'
+             }
+
+    req = requests.get(url, params=params)
+    res = json.loads(req.text)
+
+    # 視聴回数取得
+    counturl = "https://www.googleapis.com/youtube/v3/videos"
+    
+    for i in range(len(res["items"])):
+            # パラメータの設定
+            params2 = {'part': 'statistics',
+                    'id': res["items"][i]["id"]["videoId"],
+                    'key': 'AIzaSyDP1Ya1uZetxtl4D4lkbHR5MY-xPpGvVE8',
+                    'fields': 'items(statistics(viewCount))'
+            }
+            resvideo = videoInfo(res["items"][i]["id"]["videoId"],res["items"][i]["snippet"]["title"],res["items"][i]["snippet"]["channelTitle"],res["items"][i]["snippet"]["description"],json.loads(requests.get(counturl, params=params2).text)["items"][0]["statistics"]["viewCount"],"https://www.youtube.com/watch?v="+res["items"][i]["id"]["videoId"],res["items"][i]["snippet"]["thumbnails"]["high"]["url"],"Youtube")
+            video.append(resvideo)
+
+
 
 # appという名前でFlaskのインスタンスを作成
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 # 登録処理
 @app.route('/', methods=["POST"])
 def register_record():
-
-    name = request.form['name']
-
-    session.add(User(name))
+   
+    word = request.form.get("word")
+    session.query(videoInfo).delete()
+    
+    #ニコニコ動画
+    nicoURL= "https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q="+word+"&targets=title,tags&fields=title,description,contentId,thumbnailUrl,viewCounter&_sort=-viewCounter&_context=apiguide,_limit=10"
+    nico_res(nicoURL)
+    you_res(word)
+    for i in range(len(video)):
+        new_video = videoInfo(id=video[i].id , title=video[i].title, channel=video[i].channel, description=video[i].description , viewCount=video[i].viewCount , videoURL=video[i].videoURL , imageURL=video[i].imageURL , kind=video[i].kind)
+        session.add(new_video)  
+    
 
     session.commit()
-
-    return render_template("hello.html", name=name, message="登録完了しました！")
+    return redirect("/")
 
 # 取得処理
 @app.route('/', methods=["GET"])
 def fetch_record():
+    db_videoInfo = session.query(videoInfo).all()
+    
 
-    name = request.args.get('name')
+    #return render_template("index.html", name=name, message=message)
 
-    db_user = session.query(User.name).\
-        filter(User.name == name).\
-        all()
-
-    if len(db_user) == 0:
-        message = "登録されていません。"
-    else:
-        message = "登録されています。"
-
-    return render_template("index.html", name=name, message=message)
 
 if __name__ == '__main__':
     app.run()
