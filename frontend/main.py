762d8bc1694696d6298388c06ccacbafd089767d
yechentide <yechentide@gmail.com>
2021-05-15 11:00:13 +0900

    APIサーバを利用するように、ログイン機能を変更

--- frontend/main.py
+++ frontend/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-from flask import Flask, request, Response, abort, render_template
+from flask import Flask, request, Response, flash, render_template, redirect, make_response
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from datetime import datetime
 from collections import defaultdict
+import requests
+import urllib
+import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
 login_manager = LoginManager()
 login_manager.init_app(app)
-app.config['SECRET_KEY'] = "secret"
 
 class User(UserMixin):
     def __init__(self, id, name, password):
@@ -16,63 +20,86 @@ class User(UserMixin):
 
 # ログイン用ユーザー作成
 users = {
-    1: User(1, "mizoguchi", "18fi112"),
-    2: User(2, "sai", "18fi055"),
-    3: User(3, "morohashi", "17fi097"),
-    4: User(4, "ikuta", "18fi008")
+    #"mizoguchi": User(1, "mizoguchi", "18fi112"),
+    #"sai": User(2, "sai", "18fi055"),
+    #"morohashi": User(3, "morohashi", "17fi097"),
+    #"ikuta": User(4, "ikuta", "18fi008")
 }
-
-# ユーザーチェックに使用する辞書作成
-nested_dict = lambda: defaultdict(nested_dict)
-user_check = nested_dict()
-for i in users.values():
-    user_check[i.name]["password"] = i.password
-    user_check[i.name]["id"] = i.id
 
 @login_manager.user_loader
 def load_user(user_id):
-    return users.get(int(user_id))
+    for u in users.values():
+        if(str(u.id) == user_id):
+            return u
+        else:
+            print('not found this user in dict')
+
 
 @app.route('/')
 def home():
-    return Response("home: <a href='/login/'>Login</a> <a href='/protected/'>Protected</a> <a href='/logout/'>Logout</a>")
+    return redirect('/login')
 
 # ログインしないと表示されないパス
-@app.route('/protected/')
+@app.route('/home')
 @login_required
 def protected():
+    #uid = request.cookies.get('uid', None)
     return Response('''
-    protected<br />
-    <a href="/logout/">logout</a>
+    protected home page<br />
+    <a href="/logout">logout</a>
     ''')
 
 # ログインパス
-@app.route('/login/', methods=["GET", "POST"])
-def login():
-    if(request.method == "POST"):
-        # ユーザーチェック
-        if(request.form["username"] in user_check and request.form["password"] == user_check[request.form["username"]]["password"]):
-            # ユーザーが存在した場合はログイン
-            login_user(users.get(user_check[request.form["username"]]["id"]))
-            return Response('''
-            login success!<br />
-            <a href="/protected/">protected</a><br />
-            <a href="/logout/">logout</a>
-            ''')
-        else:
-            return abort(401)
-    else:
+@app.route('/login', methods=["GET", "POST"])
+def test():
+    if(request.method == "GET"):
         return render_template("login.html")
+    
+    if(request.method == "POST"):
+        user_name = request.form.get('username')
+        user_pass = request.form.get('password')
+        url = 'http://127.0.0.1:8008/token'
+        params = {
+            'username': user_name,
+            'password': user_pass,
+        }
+        params = urllib.parse.urlencode(params)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        responseFromAPI = requests.post(url=url, data=params, headers=headers)
+        
+        if(responseFromAPI.status_code == 200):
+            
+            if user_name not in users:
+                print('add user: ' + user_name)
+                users[user_name] = User(len(users)+1, user_name, user_pass)
+            
+            tokens = json.loads(responseFromAPI.text)  # 'access_token', 'refresh_token', 'token_type'
+            login_user(users.get(user_name))
+            
+            response = make_response(redirect('/home'))
+            max_age = 60 * 60 * 24 # 24h
+            expires = int(datetime.now().timestamp()) + max_age
+            response.set_cookie(
+                'token',
+                value=tokens['access_token'],
+                max_age=max_age,
+                expires=expires,
+                path='/',
+                secure=None,
+                httponly=False
+            )
+            
+            return response
+        else:
+            flash("認証失敗しました", "failed")
+            return render_template("login.html")
 
 # ログアウトパス
-@app.route('/logout/')
+@app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return Response('''
-    logout success!<br />
-    <a href="/login/">login</a>
-    ''')
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8000,debug=True)
