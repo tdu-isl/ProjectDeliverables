e1843b4adc6d7277b5cd575159c0527ba7e7aea9
Yukyan <yukyanjpn@gmail.com>
2021-05-19 02:37:20 +0900

    Add: デザインなど

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('profile/<str:username>/', views.user_profile, name='profile')
+    path('<str:username>/', views.user_profile, name='profile')
 ]
