d480f08c11391a01d9543e241e111cf87cc34b5c
Yukyan <yukyanjpn@gmail.com>
2021-05-19 15:00:44 +0900

    Delete実装途中

--- post/urls.py
+++ post/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # path('post_create/', views.post_create, name='post_create'),
     path('', views.show_top, name='top'),
     path('create/', views.CreateView.as_view(), name='create'),
+    path('delete/<int:pk>/', views.DeleteView.as_view(), name='delete'),
     path('home/', views.IndexView.as_view(), name='home')
 ]
--- post/views.py
+++ post/views.py
@@ -1,12 +1,22 @@
 from post.forms import PostCreateForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import Post
 
 
 class IndexView(generic.ListView):
     model = Post
+
+
+class DeleteView(UserPassesTestMixin, generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('accounts:profile')
+
+    def test_func(self):
+        user = self.request.user
+        return user.pk == self.get_object().account.pk or user.is_superuser
 
 
 class CreateView(LoginRequiredMixin, generic.edit.CreateView):
