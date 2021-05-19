a5c0eb736f29a5ccf10e0d9b1cab95b445e70e98
Yukyan <yukyanjpn@gmail.com>
2021-05-19 16:47:07 +0900

    Change: staticfileの設定

--- psemi2021_app_a/settings.py
+++ psemi2021_app_a/settings.py
@@ -155,6 +155,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 try:
     from .local_settings import *
