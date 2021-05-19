d0f2638540511ef190f354661f224f1dc097ab51
Yukyan <yukyanjpn@gmail.com>
2021-05-19 19:23:46 +0900

    tekito2

--- psemi2021_app_a/settings.py
+++ psemi2021_app_a/settings.py
@@ -157,7 +157,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
