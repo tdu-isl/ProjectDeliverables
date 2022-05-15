42e73d9c3a7c223cef552e8996f0108aaf032245
su__ma <81449758+suzuma110@users.noreply.github.com>
2022-05-15 14:09:12 +0900

    Suzuki's Second Commit

--- html_prototype/app/app.py
+++ html_prototype/app/app.py
@@ -2,18 +2,27 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
+def top():
+    return render_template("top.html")
+
+
+@app.route("/shindan")
 def shindan():
     return render_template("shindan_page.html")
 
-@app.route("/result", methods=["POST"])
+
+@app.route("/shindan/result", methods=["POST"])
 def result():
     field = request.form["field"]
-    return render_template("shindan_result.html", message = field)
+    return render_template("shindan_result.html", message=field)
 
-@app.route("/result/twitter_coop")
+
+@app.route("/shindan/result/twitter_coop")
 def twitter_coop():
     return render_template("twitter_coop.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
