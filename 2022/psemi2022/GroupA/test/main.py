3307213441ab73f8dc149f568bea6bd150ceeaa3
t-star <raiki.togawa@paiza.co.jp>
2022-05-16 12:44:03 +0900

    add test

---
+++ 2022/psemi2022/GroupA/test/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    with open(".sitekey", mode="r") as f:
+        sitekey = f.read()
+    with open(".secretkey", mode="r") as f:
+        secretkey = f.read()
+
+    print(request.form.get("g-recaptcha-response", "None"))
+    if request.form.get("g-recaptcha-response", None) is not None:
+        url = "https://www.google.com/recaptcha/api/siteverify"
+        body = {
+            "secret": secretkey,
+            "response": request.form.get("g-recaptcha-response")
+        }
+        print(requests.post(url, data=body).json())
+
+    return render_template("index.html", sitekey=sitekey, secretkey=secretkey)
+
+
+if __name__ == "__main__":
+    app.run(port=8080, host="localhost", debug=True)
