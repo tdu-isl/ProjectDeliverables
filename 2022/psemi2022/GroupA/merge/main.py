3722024dcb098f8b26ec9b4c02d44ca90818dba0
t-star <raiki.togawa@paiza.co.jp>
2022-05-22 01:25:03 +0900

    implement challengeResultModal

--- 2022/psemi2022/GroupA/merge/main.py
+++ 2022/psemi2022/GroupA/merge/main.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template, request
 import requests
 
 
+ADMITTED_MSG = "認証に成功しました。"
+DENIED_MSG = {
+    "invalid-input-response": "認証に失敗しました。\nトークンが無効または不正です。",
+    "bad-request": "認証に失敗しました。\nリクエストが無効または不正です。",
+    "timeout-or-duplicate": "認証に失敗しました。\nトークンが古すぎるか、すでに使われています。",
+    "unknown": "認証に失敗しました。"
+}
 app = Flask(__name__)
 
 
@@ -12,20 +19,32 @@ def index():
     with open(".secretkey", mode="r") as f:
         secretkey = f.read()
 
-    print(request.form.get("g-recaptcha-response", "None"))
+    challenge_result = None
     if request.form.get("g-recaptcha-response", None) is not None:
         url = "https://www.google.com/recaptcha/api/siteverify"
         body = {
             "secret": secretkey,
             "response": request.form.get("g-recaptcha-response")
         }
-        print(requests.post(url, data=body).json())
+        resp = requests.post(url, data=body)
+        if resp.status_code == requests.codes.ok:
+            if resp.json()["success"]:
+                challenge_result = ADMITTED_MSG
+            else:
+                challenge_result = DENIED_MSG.get(resp.json()["error-codes"][0], DENIED_MSG["unknown"])
 
-    return render_template("index.html", sitekey=sitekey, secretkey=secretkey)
+    return render_template("index.html", sitekey=sitekey, secretkey=secretkey, challenge_result=challenge_result, showup=challenge_result is not None)
 
-@app.route("/detail",methods=["POST", "GET"])
+
+@app.route("/detail")
 def detail():
     return render_template("detail.html")
+
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
 
 # @app.route("/test")
 # def _index():
