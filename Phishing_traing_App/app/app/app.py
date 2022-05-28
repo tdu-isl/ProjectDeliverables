20eb05169a032de8114ccdd1a94e0c9bcf03cf48
su__ma <81449758+suzuma110@users.noreply.github.com>
2022-05-28 16:11:42 +0900

    最終更新

--- Phishing_traing_App/app/app/app.py
+++ Phishing_traing_App/app/app/app.py
@@ -7,13 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///psemi.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class UserInformation(db.Model):
     __tablename__ = 'user_information'
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(100))
     password = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.now, onupdate=datetime.now)
 
 
 @app.route("/")
@@ -29,6 +31,7 @@ def explain():
 @app.route("/shindan")
 def shindan():
     return render_template("shindan_page.html")
+
 
 @app.route("/member")
 def member():
@@ -50,6 +53,7 @@ def twitter_coop():
         username = request.form['username']
     return render_template("twitter_coop.html", username=username)
 
+
 @app.route("/shindan/result/twitter_coop/update", methods=["POST", "GET"])
 def update():
     if request.method == 'GET':
@@ -60,15 +64,18 @@ def update():
         form_password = request.form.get('password')
 
         user_information = UserInformation(
-            userid = form_userid,
-            password = form_password
+            userid=form_userid,
+            password=form_password
         )
         db.session.add(user_information)
         db.session.commit()
         return redirect(redirect_link, code=301)
 
+
 @app.route("/shindan/result/twitter_coop/finish")
 def finish():
-    userid = UserInformation.query.order_by(UserInformation.created_at.desc()).first()
-    password = UserInformation.query.order_by(UserInformation.created_at.desc()).first()
+    userid = UserInformation.query.order_by(
+        UserInformation.created_at.desc()).first()
+    password = UserInformation.query.order_by(
+        UserInformation.created_at.desc()).first()
     return render_template("finish.html", userid=userid.userid, password=password.password)
